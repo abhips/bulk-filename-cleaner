89c3b14d85a4534e3d3110f91073bb653c74a560
Abhilash PS <developer.abhilash@gmail.com>
2020-02-03 02:29:48 +0530

    remove Enum class YesNo

--- helper_enums.py
+++ helper_enums.py
@@ -7,17 +7,10 @@
 
 from enum import Enum, unique
 
+
 @unique
 class FileNameCase(Enum):
     """enum to represent case of the file name"""
     LOWER = 1
     UPPER = 2
     TITLE = 3
-
-@unique
-class YesNo(Enum):
-    """
-        enum to represent yes no
-    """
-    NO = 0
-    YES = 1
