0b36ee02d62094cfa692f6495b940d75de31ed59
Abhilash PS <developer.abhilash@gmail.com>
2020-02-03 02:30:56 +0530

    add function "replace_multiple_character_occurances"

--- script_utils.py
+++ script_utils.py
@@ -5,7 +5,9 @@
     :license: The MIT License
 """
 
+import re
 import sys
+
 
 class ScriptUtils(object):
     """
@@ -24,29 +26,34 @@ class ScriptUtils(object):
     """
 
     def check_exit_code(self, user_input):
-
         if user_input.lower() == 'exit':
             sys.exit(0)
-
 
     """
        function to give a formatted response to the users
     """
 
-    def talk_to_user(self, message, start_character='', initial_space=0):
-        message = start_character + ' '*initial_space + message
+    def talk_to_user(self, message='', start_character='', initial_space=0):
+        message = str(start_character) + ' ' * initial_space + message
         print(message)
 
     """
         function to ask question and return the answer
     """
+
     def ask_question(self, question, start_character='', initial_space=0):
         print()
-        question = start_character + ' '*initial_space + question
+        question = start_character + ' ' * initial_space + question
         answer = input(question)
         self.check_exit_code(answer)
 
         return answer
 
-    def print_exception(self):
-        pass
+    """
+        function to replace multiple occurrences of a character by a single character.
+    """
+
+    def replace_multiple_character_occurances(self, string, character):
+        reg_pattern = character + '{2,}'
+        string = re.sub(reg_pattern, character, string)
+        return string
